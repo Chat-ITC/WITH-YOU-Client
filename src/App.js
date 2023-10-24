261bb2af4aefcfe255360e257f29acd5598ea770
SiHoon61 <dlatlgns000@gmail.com>
2023-10-24 21:27:38 +0900

    Add: 사진 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import Bottom from './components/Bottom';
 import Home from './pages/Home/Home';
+
+//pages
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
 
   return (  
