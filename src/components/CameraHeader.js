fe8212a143866c48f43807512a83526837b1787d
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-25 02:40:55 +0900

    Add: 리덕스 주석 달기

--- src/components/CameraHeader.js
+++ src/components/CameraHeader.js
@@ -1,20 +1,25 @@
+//style
+import { CameraHeaderTop } from './style';
+//library
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+//store.js에서 'toggleAsideVisibility'라는 액션을 import
 import { toggleAsideVisibility } from '../store';
 
-import { CameraHeaderTop } from './style';
 
 const CameraHeader = () => {
+  //useSelector 훅을 사용해서 visibility슬라이스의 isAsideVisible 상태를 추출한다.
   const isAsideVisible = useSelector((state) => state.visibility.isAsideVisible);
+  //useDispatch를 호출하여 dispatch함수를 가져와 이 함수를 이용해 리덕스 스토어에 액션을 보낼 수 있다.
   const dispatch = useDispatch();
 
-  const toggleAsideVisibilityHandler = () => {
+  const AsideHandler = () => {
     dispatch(toggleAsideVisibility());
   };
 
   return (
     <CameraHeaderTop style={{ left: isAsideVisible ? '0' : '300px' }}>
-      <div onClick={toggleAsideVisibilityHandler}>눌러</div>
+      <div onClick={AsideHandler}>눌러</div>
     </CameraHeaderTop>
   );
 };
--- src/store.js
+++ src/store.js
@@ -1,15 +1,19 @@
-// store.js
+// store.js - 리덕스 툴킷을 사용하여 store를 설정하고 구성하는 역할
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-// 초기 상태 정의
+// 초기 상태 정의 useState(false)와 같은 역할
 const initialState = {
   isAsideVisible: false,
 };
 
-// Redux Toolkit을 사용하여 슬라이스를 생성
+// Redux Toolkit을 사용하여 슬라이스(객체)를 생성
+//슬라이스는 상태와 상태 변경을 다루는데 필요한 여러 정보를 묶어주는 역할
+//슬라이스(객체)를 생성하고 visibilitySlice.reducers로 상태와 액션을 얻을 수 있다.
 const visibilitySlice = createSlice({
+  //'visibility': 해당 슬라이스의 이름
   name: 'visibility',
   initialState,
+  //reducers 객체안에 액션 및 액션 생성자함수 정의
   reducers: {
     toggleAsideVisibility: (state) => {
       state.isAsideVisible = !state.isAsideVisible;
@@ -25,8 +29,10 @@ const rootReducer = {
   visibility: visibilitySlice.reducer,
 };
 
-// 스토어 생성
+// configureStore 함수를 사용하여 리덕스 스토어 생성하여
+//rootReducer로 지정된 리듀서들을 사용한다고 설정하는 부분
 const store = configureStore({
+  //rootReducer: store.js에서 생성된 리듀서들을 하나로 합쳐주는 편리한 객체
   reducer: rootReducer,
 });
 
