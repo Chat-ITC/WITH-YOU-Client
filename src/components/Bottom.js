e29d5699e26569faf1961f389ee2e388acf80a70
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-25 23:44:10 +0900

    Add: Bottom Bottom 완성

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -13,35 +13,55 @@ import Community from '../assets/Community.png';
 import UserIcon from '../assets/userIcon2.png';
 
 const Bottom = () => {
-  let activeStyle = {
-    opacity: 1
-  }
   return (
       <BottomFix>
         <Ul>
-          <NavLink to="/home"
-          style={({isActive}) =>
-            isActive ? activeStyle : undefined
-          }>
+          <NavLink to="/home">
+            {/*NavLink를 사용하면 활성화된 페이지의 isActive값이 true가 된다.*/}
+          {({ isActive }) => (
             <li>
-              <ImgBtn src={Home} alt="홈" />
+              <ImgBtn
+                src={Home}
+                alt="홈"
+                /*isActive가 true이면 1 false이면 0.3*/
+                style={{ opacity: isActive ? 1 : 0.3 }}
+              />
             </li>
-          </NavLink>
+          )}
+        </NavLink>
           <NavLink to="/scrap">
+          {({ isActive }) => (
             <li>
-                <ImgBtn src={Star} alt="스크랩" />
+              <ImgBtn
+                src={Star}
+                alt="스크랩"
+                style={{ opacity: isActive ? 1 : 0.3 }}
+              />
             </li>
-          </NavLink>
-          <NavLink to="/community">
+          )}
+        </NavLink>
+        <NavLink to="/community">
+          {({ isActive }) => (
             <li>
-                <ImgBtn src={Community} alt="커뮤니티" />
+              <ImgBtn
+                src={Community}
+                alt="커뮤니티"
+                style={{ opacity: isActive ? 1 : 0.3 }}
+              />
             </li>
-          </NavLink>
-          <NavLink to="/mypage">
+          )}
+        </NavLink>
+        <NavLink to="/mypage">
+          {({ isActive }) => (
             <li>
-              <ImgBtn src={UserIcon} alt="내 정보" />
+              <ImgBtn
+                src={UserIcon}
+                alt="내 정보"
+                style={{ opacity: isActive ? 1 : 0.3 }}
+              />
             </li>
-          </NavLink>
+          )}
+        </NavLink>
         </Ul>
       </BottomFix>
   );
