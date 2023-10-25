ac06e08c5cccc916eddb2c77fb6e8331b72bca49
SiHoon61 <dlatlgns000@gmail.com>
2023-10-25 22:59:14 +0900

    Add: home페이지에 큰 카메라 버튼 추가

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -14,30 +14,30 @@ import UserIcon from '../assets/userIcon2.png';
 
 const Bottom = () => {
   return (
-      <BottomFix>
-        <Ul>
-          <Link to="/home">
-            <li>
-              <ImgBtn src={Home} alt="홈" />
-            </li>
-          </Link>
-          <Link to="/scrap">
-            <li>
-                <ImgBtn src={Star} alt="스크랩" />
-            </li>
-          </Link>
-          <Link to="/community">
-            <li>
-                <ImgBtn src={Community} alt="커뮤니티" />
-            </li>
-          </Link>
-          <Link to="/mypage">
-            <li>
-              <ImgBtn src={UserIcon} alt="내 정보" />
-            </li>
-          </Link>
-        </Ul>
-      </BottomFix>
+    <BottomFix>
+      <Ul>
+        <Link to="/home">
+          <li>
+            <ImgBtn src={Home} alt="홈" />
+          </li>
+        </Link>
+        <Link to="/scrap">
+          <li>
+            <ImgBtn src={Star} alt="스크랩" />
+          </li>
+        </Link>
+        <Link to="/community">
+          <li>
+            <ImgBtn src={Community} alt="커뮤니티" />
+          </li>
+        </Link>
+        <Link to="/mypage">
+          <li>
+            <ImgBtn src={UserIcon} alt="내 정보" />
+          </li>
+        </Link>
+      </Ul>
+    </BottomFix>
   );
 }
 
--- src/components/style.js
+++ src/components/style.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import openeye from '../assets/openEye.png';
 import closeeye from '../assets/closeEye.png';
 import fillStar from '../assets/FillStar.png';
+import camera from '../assets/camera.png'
 import emptyStar from '../assets/clip.png';
 
 //Bottom
@@ -16,7 +17,7 @@ export const BottomFix = styled.div`
 `;
 
 
-//CameraHeader
+//CameraItem
 export const CameraItemContainer = styled.div`
   width: 300px;
   height: 100px;
@@ -76,7 +77,29 @@ export const DateContainer = styled.div`
   margin: 2px 0px 1px 0px;
 `
 
-//RightTop
+//BodySection
+export const CameraButtonContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 160px;
+`;
+export const CameraBigButton = styled.div`
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-image: none;
+  background-image: url(${camera});
+  width: 200px;
+  height: 200px;
+`;
+export const CameraPhrases = styled.div`
+  font-size: 20px;
+`
+
+
+//CameraHeader
 export const CameraHeaderTop = styled.div`
   display: flex;
   justify-content: space-between;
