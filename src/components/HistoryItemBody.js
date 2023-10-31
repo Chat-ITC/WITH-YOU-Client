22d0e40ba19b2329d58c4e297827c3236b4c4d74
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-11-01 03:02:31 +0900

    Add: HistoryBody 컴포넌트 추가

---
+++ src/components/HistoryItemBody.js
@@ -0,0 +1,44 @@
+//style
+import {
+    CameraItemContainer,
+    CameraItemTitle,
+    CameraItemScrap,
+    CameraItemBody,
+    CameraItemBottom,
+    CameraDateContainer,
+} from './style';
+
+//library
+import React from 'react';
+import { selectId, historyBody, scrapId } from '../store';
+import { useDispatch } from 'react-redux';
+import axiosInstance from '../utils/axiosInterceptor/axiosInterceptor';
+import ReactMarkdown from 'react-markdown';
+
+const HistoryItemBody = (props) => {
+    return (
+        <>
+            {(props.title.includes(props.searchWord) || props.body.includes(props.searchWord)) && (
+                <CameraItemContainer>
+                    <CameraItemTitle>
+                        {props.title}
+                    </CameraItemTitle>
+                    <CameraItemBody>
+                        {props.body}
+                    </CameraItemBody>
+                    <CameraItemBottom>
+                        <CameraDateContainer>
+                            <CameraItemScrap $done={props.$scrap} />
+                            <ReactMarkdown>
+                            </ReactMarkdown>
+                            {props.date}
+                        </CameraDateContainer>
+                    </CameraItemBottom>
+
+                </CameraItemContainer>
+            )}
+        </>
+    );
+}
+
+export default HistoryItemBody;
