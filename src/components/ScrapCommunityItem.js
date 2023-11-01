01e94300675e6d37459688152e3849da5eacdada
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-11-01 18:37:30 +0900

    Remove: ScrapCommunityItem 필요없는 코드 제거 (dispatch)

--- src/components/ScrapCommunityItem.js
+++ src/components/ScrapCommunityItem.js
@@ -21,17 +21,9 @@ import Chat from '../assets/chat.png';
 
 //library
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import axiosInstance from '../utils/axiosInterceptor/axiosInterceptor';
-import { selectId, historyBody, scrapId } from '../store';
 
 const ScrapCommunityItem = (props) => {
-    console.log("로그 확이", props);
-    const dispatch = useDispatch();
-    // const [picture, setPicture] = useState(props.$picture);
-
-    
-    
+    console.log("콘솔: ", props);
     return (
         <>
             {(props.title.includes(props.searchWord) || props.content.includes(props.searchWord)) && (
@@ -49,14 +41,14 @@ const ScrapCommunityItem = (props) => {
                             {props.content}
                         </CommunityItemBody>
                         <CommunityItemBottom>
-                            {props.like > 0 && (
+                            {/* {props.like > 0 && (
                                 <LikeContainer>
                                     <LikeImg src={Like} alt="좋아요 버튼" />
                                     <LikeContent>
                                         {props.like}
                                     </LikeContent>
                                 </LikeContainer>
-                            )}
+                            )} */}
                             {props.commentCount > 0 && (
                                 <ChatContainer>
                                     <ChatImg src={Chat} alt="댓글 버튼" width="14" />
@@ -67,7 +59,7 @@ const ScrapCommunityItem = (props) => {
                             )}
                             <CommunityDateContainer>
                                 <CommunityItemScrap $done={props.$scrap} />{/*isScrap인지 확인 */}
-                                {props.createdDate}
+                                {props.localDateTime}
                             </CommunityDateContainer>
                         </CommunityItemBottom>
                     </CommunityItemContainer>
