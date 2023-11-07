cb94c993625e42fc0d1fc90d4611d80327cb8334
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-11-07 18:18:41 +0900

    Modify: Modal 제목 변경 모달에서 요청사항 입력 모달로 변경

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,6 +7,7 @@ import {
   ModalSetBtn,
   ModalCloseBtn,
   NickBtnBox,
+  ModalInput,
 } from "./style";
 //library
 import React, {useState} from "react";
@@ -15,23 +16,26 @@ import axiosInstance from '../utils/axiosInterceptor/axiosInterceptor';
 //components
 import TextInput from './TextInput';
 
-const Modal = ({onClose}) => {
+const Modal = ({onClose, onContentChange}) => {
   const navigate = useNavigate();
   //제목 설정
   const [content, setContent] = useState('');
 
   const handleChangeContent = async () => {
-    const title = { content: content };
-    console.log(`제목: ${content}`);
-    console.log(`제목 Json: `, title)
-    try {
-      await axiosInstance.patch('/member/update', title);
-      alert("제목이 성공적으로 변경되었습니다.");
-
-    } catch (error) {
-      alert("세션이 만료되었습니다. 다시 로그인해 주세요");
-      navigate("/");      
+    if(content.trim() !== ""){
+      onContentChange(content);
     }
+    // const title = { content: content };
+    // console.log(`제목: ${content}`);
+    // console.log(`제목 Json: `, title)
+    // try {
+    //   await axiosInstance.patch('/member/update', title);
+    //   alert("제목이 성공적으로 변경되었습니다.");
+
+    // } catch (error) {
+    //   alert("세션이 만료되었습니다. 다시 로그인해 주세요");
+    //   navigate("/");      
+    // }
   };
 
   
@@ -42,15 +46,19 @@ const Modal = ({onClose}) => {
                 }}></Mask>
       <ModalBody>
           <Content>
-            <ModalTItle>제목을 변경하세요</ModalTItle>
+            <ModalTItle>추가할 요구사항을 입력하세요</ModalTItle>
             <TextInput
-              placeholder={"제목을 변경하세요"}
+              placeholder={"요구사항 입력"}
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
+            <ModalInput>질문을 상세화하여 요청해주세요!</ModalInput>
             <NickBtnBox>
               <ModalSetBtn
-                onClick={handleChangeContent}>
+                onClick={()=>{
+                  handleChangeContent();
+                  onClose();
+                }}>
                 설정
               </ModalSetBtn>
               <ModalCloseBtn 
