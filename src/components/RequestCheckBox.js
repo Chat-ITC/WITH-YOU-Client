6b1357b018d819a5e4afd78c3af34ab9167edbbb
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-11-07 18:19:14 +0900

    Refactor: RequestCheckBox 공백 제거

--- src/components/RequestCheckBox.js
+++ src/components/RequestCheckBox.js
@@ -10,7 +10,7 @@ import checkBoxImg from '../assets/checkbox.png';
 import UnCheckBoxImg from '../assets/uncheckbox.png';
 
 const RequestCheckBox = ({onClick, content, id, $done }) => {
- 
+
   const imgRadio = $done ? checkBoxImg : UnCheckBoxImg;
   const handleCheckBoxClick = () => {
     onClick(id); // 클릭 시 해당 아이디를 사용하여 onClick함수 호출
